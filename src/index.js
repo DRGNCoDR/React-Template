255a9c51609a2ab117871c29b66dd1cd220438ab
DRGNCoDR <rayfradella@yahoo.com>
2025-07-31 00:58:24 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import App from './app';
+import React from "react"
+import App from "./app"
 
-import { createRoot } from 'react-dom/client';
+import {createRoot} from "react-dom/client"
 
-const domNode = document.getElementById('root');
-const root = createRoot(domNode);
+const domNode = document.getElementById("root")
+const root = createRoot(domNode)
 
-root.render(<App />);
+root.render(<App />)
