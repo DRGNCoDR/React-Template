eef4324f8b2a3f3d49bca70daaeb122d0dd79765
DRGNCoDR <rayfradella@yahoo.com>
2025-07-31 01:02:55 -0400

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,24 +4,31 @@ import babel from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
+
 export default {
   input: "src/index.js",
+  
   output: {
     file: "dist/bundle.js",
     format: "iife",
     sourcemap: true,
   },
+  
   plugins: [
     nodeResolve({
       extensions: [".js"],
     }),
+    
     replace({
-      'process.env.NODE_ENV': JSON.stringify( 'development' )
+      'process.env.NODE_ENV': JSON.stringify("development")
     }),
+    
     babel({
       presets: ["@babel/preset-react"],
     }),
+    
     commonjs(),
+    
     serve({
       open: true,
       verbose: true,
@@ -29,6 +36,7 @@ export default {
       host: "localhost",
       port: 3000,
     }),
+    
     livereload({ watch: "dist" }),
   ]
 };
